6597c0d7dd4e36c3aa7883a6e3aaf801b49bd8f6
matyasbp <matyasm@gmail.com>
2016-05-08 03:42:03 +0200

    Window size changed and button/label relocated.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 
 public class Controller {
+
+    public Label myLabel;
+    public void setLabel(ActionEvent actionEvent) {
+        myLabel.setText("hi");
+    }
 }
